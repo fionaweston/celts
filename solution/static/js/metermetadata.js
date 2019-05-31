74e4b8df969aad5aeac8939181375f55edda66fb
Scott Mac McEachern <the_fixer@hotmail.com>
2019-05-30 20:04:18 -0700

    Started meter metadata

---
+++ solution/static/js/metermetadata.js
@@ -0,0 +1,11 @@
+/* metermetadata.js
+
+Contains functionality to display the metadata for the selected meter
+*/
+
+console.log("--> metermetadata.js");
+
+
+function updateMeterMetadata(){
+    console.log("test");
+}
