1eae86fbee1344492b426fab92cde61b4130b2bf
David Mathews <david.g.mathews.edu@gmail.com>
2019-06-01 09:46:29 -0700

    enhanced map, geojson

--- solution/static/js/districtmap.js
+++ solution/static/js/districtmap.js
@@ -4,86 +4,115 @@ Contains functionality to display the meters of the selected agency on a map
 
 
 console.log("-> districtmap.js");
+function chooseColor(district) {
+    var color="red";
+    if (district=="Valley") { color="green"; 
+      
+    } else {if (district=="Southern") {color="blue";
+      
+    } else {if (district=="Western") {color="yellow";
+      
+    } else {if (district=="Hollywood") {color="purple";
+      
+    } else color="red";}}}
+    return color;
+  
+    }
 
 
 function initializeMap(sourceAgencyDetails){
-    /* Creates the map to display the top 100 meters for the agency provided.
-
-    Accepts : sourceAgencyDetails (dictionary) contains metadata for selected agency used to populate chart
-                id: (string) unique identifier for the agency the details are for
-                year: (int) year for which the agency details are for
-                summarydayofweek: (List)
-                    day: (string) Day of week; 0 Monday, 1 Tuesday, 2 Wednesday, 3 Thursday, 4 Friday, 5 Saturday, 6 Sunday
-                    count: (int)
-                summaryhour: (List)
-                    hour: (string)
-                    count: (int)
-                meters: (list) List of the top meters, by count, for the agency
-                    location: (string)
-                    id: (string)
-                    count: (int)
-                    latitude: (number)
-                    longitude: (number)
     
-    Returns : undefined
-    */
-
-    console.log("--> initializeMap");
-
-    // let mapDiv = d3.select("#map");
-
     // console.log(mapDiv);
     mapboxgl.accessToken = API_KEY;
     var map = new mapboxgl.Map({
     container: 'map', // container id
     style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-    center: [-118.353650, 34.113055], // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    center: [-118.3319447,34.0098852], // starting position [lng, lat]
+    // los angeles: 34.0398852,-118.3319447
+    zoom: 8.7 // starting zoom
+    });
+    
+    
+    
+    sourceAgencyDetails.meters.forEach(meter=>{
+        var marker = new mapboxgl.Marker({
+        
+        })
+        .setLngLat([meter.longitude,meter.latitude]).addTo(map);    
+    
+    
+    })
+
+    map.on('load', function () {
+        d3.json("static/data/boundaries.geojson").then(gj=>gj.features.forEach(district=>{
+            
+            map.addLayer({
+                'id': district.properties.District,
+                'type': 'fill',
+                'source': {
+                'type': 'geojson',
+                'data': district
+                },
+                'layout': {},
+                'paint': {
+                'fill-color': chooseColor(district.properties.District),
+                'fill-opacity': 0.5
+                }
+                });
+                
+
+        }));
+    });
+    
+}
+ 
+        
+    
+
+function updateMap(sourceAgencyDetails){
+    
+    // console.log(mapDiv);
+    mapboxgl.accessToken = API_KEY;
+    var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+    center: [-118.3319447,34.0098852], // starting position [lng, lat]
+    // 34.067237,-118.3167928
+    // -118.353650, 34.113055
+    zoom: 8.7 // starting zoom
     });
     console.log("checking map api object")
     
     
-    sourceAgencyDetails.meters.forEach(i=>{
+    sourceAgencyDetails.meters.forEach(meter=>{
         var marker = new mapboxgl.Marker({
         
         })
-        .setLngLat([i.longitude,i.latitude]).addTo(map);
+        .setLngLat([meter.longitude,meter.latitude]).addTo(map);
     
     
     
     })
-    // var marker = new mapboxgl.Marker({
-    //     draggable: true
-    //     })
-    //     .setLngLat([0, 0])
-    //     .addTo(map);
-    
-    // 34.113055, -118.353650
 
+    map.on('load', function () {
+        d3.json("static/data/boundaries.geojson").then(gj=>gj.features.forEach(district=>{
+            
+            map.addLayer({
+                'id': district.properties.District,
+                'type': 'fill',
+                'source': {
+                'type': 'geojson',
+                'data': district
+                },
+                'layout': {},
+                'paint': {
+                'fill-color': chooseColor(district.properties.District),
+                'fill-opacity': 0.5
+                }
+                });
+                
+
+        }));
+    });
 }
 
-
-function updateMap(sourceAgencyDetails){
-    /* Updates the existing map with the new agency.
-
-    Accepts : sourceAgencyDetails (dictionary) contains metadata for selected agency used to populate chart
-                id: (string) unique identifier for the agency the details are for
-                year: (int) year for which the agency details are for
-                summarydayofweek: (List)
-                    day: (string) Day of week; 0 Monday, 1 Tuesday, 2 Wednesday, 3 Thursday, 4 Friday, 5 Saturday, 6 Sunday
-                    count: (int)
-                summaryhour: (List)
-                    hour: (string)
-                    count: (int)
-                meters: (list) List of the top meters, by count, for the agency
-                    location: (string)
-                    id: (string)
-                    count: (int)
-                    latitude: (number)
-                    longitude: (number)
-
-    Returns : undefined 
-    */
-
-    console.log("--> updateMap");
-}
