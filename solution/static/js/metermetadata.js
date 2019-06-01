b5f09b94fb87bbb40ce80101ec5d60b9ca797378
Scott Mac McEachern <the_fixer@hotmail.com>
2019-05-31 17:20:00 -0700

    Completed meter metadata chart

--- solution/static/js/metermetadata.js
+++ solution/static/js/metermetadata.js
@@ -9,11 +9,15 @@ console.log("--> metermetadata.js");
 //- Constants
 const METADATA_DivName = "#mapMetadata"; //name of div to host the SVG
 
-const METADATA_SvgHeight = 500; //Height of the SVG; should match map
+const METADATA_SvgHeight = 400; //Height of the SVG; should match map
 
 const METADATA_TopMargin = 5;//Margin at the top of the SVG
 
-const METADATA_SideMargin = 5;//margin on the side of SVG
+const METADATA_BottomMargin = 30; //Margin at the bottom of SVG
+
+const METADATA_LeftMargin = 5;//margin on the left side of SVG
+
+const METADATA_RightMargin = 5;//margin on the right side of SVG
 
 const METADATA_TableMeterID = "#meterMetadataTableID"; //ID of the td that displays meter ID
 
@@ -21,6 +25,9 @@ const METADATA_TableLocation = "#meterMetadataTableLocation"; //ID of the td tha
 
 const METADATA_TableCitations = "#meterMetadataTableCitation"; //ID of the td that displays citation count
 
+const METADATA_yColumnName = "vehichemake"; //Name of the column that contains y information
+
+const METADATA_xColumnName = "count"; //Name of the column that contains x information
 
 
 
@@ -34,25 +41,6 @@ function initializeMeterMetadata(){
     */
 
     console.log("--> initializeMeterMetadata");
-
-
-    //- Remove Existing SVG
-    removeExistingSvg(METADATA_DivName);
-
-
-    //- Get Size of SVG
-    let svgWidth = getDivWidth(METADATA_DivName);
-
-
-    //- Create SVG Container
-    let svgContainer = d3.select(METADATA_DivName)
-                            .append("svg")
-                            .attr("height", METADATA_SvgHeight)
-                            .attr("width", svgWidth);
-    
-    
-    
-    
 
 }
 
@@ -69,17 +57,21 @@ function clearMeterMetadata(){
     console.log("--> clearMetaMetadata");
 
 
-    //- Update Metadata
+    //- Clear Metadata
     d3.select(METADATA_TableMeterID).text("");
 
     d3.select(METADATA_TableLocation).text("");
 
     d3.select(METADATA_TableCitations).text("");
+
+
+    //- Remove Existing SVG
+    removeExistingSvg(METADATA_DivName);
 }
 
 
 function updateMeterMetadata(sourceMeterDetails, meterLocation, citationCount){
-    /*
+    /* Creates the chart of the vehicle make; replaces existing
 
     Accepts : sourceMeterDetails: (dictionary) contains details on selected meter
                 id: (string) unique identifier of meter
@@ -101,4 +93,85 @@ function updateMeterMetadata(sourceMeterDetails, meterLocation, citationCount){
 
     d3.select(METADATA_TableCitations).text(citationCount.toLocaleString());
 
+
+
+    //- Remove Existing SVG
+    removeExistingSvg(METADATA_DivName);
+
+
+    //- Get Size of SVG
+    let svgWidth = getDivWidth(METADATA_DivName);
+
+
+    //- Create SVG Container
+    let svgContainer = d3.select(METADATA_DivName)
+                            .append("svg")
+                            .attr("height", METADATA_SvgHeight)
+                            .attr("width", svgWidth);
+    
+    //- Create Chart Group within SVG
+    //  Shift based on the margins
+    let svgChartGroup = svgContainer.append("g")
+                            .attr("transform", `translate(${METADATA_LeftMargin}, ${METADATA_TopMargin})`); 
+
+
+    //- Get Chart Area
+    //  Excludes the margins
+    let chartHeight = (METADATA_SvgHeight - METADATA_TopMargin - METADATA_BottomMargin);
+
+    let chartWidth = (svgWidth - METADATA_RightMargin - METADATA_LeftMargin);
+    
+
+    //- Prepare Y: Vehicle Make
+    let yScale = d3.scaleBand()
+                    .range([chartHeight, 0])
+                    .domain(sourceMeterDetails.summarymake.map(d => d.vehiclemake))
+                    .padding(0.1);
+
+    let yAxis = d3.axisLeft(yScale);
+
+
+    // Remove labels on y axis; using different method
+    yAxis.tickValues([]);
+
+    //- Prepare X: Count
+    let xScale = d3.scaleLinear()
+                    .domain([d3.min(sourceMeterDetails.summarymake, d => d[METADATA_xColumnName]) - 0.5, d3.max(sourceMeterDetails.summarymake, d => d[METADATA_xColumnName])  ])
+                    .range([0, chartWidth]);
+
+    let xAxis = d3.axisBottom(xScale);
+
+    
+    //- Create Chart
+    svgChartGroup.selectAll("meterBar")
+        .data(sourceMeterDetails.summarymake)
+        .enter()
+        .append("rect")
+        .attr("class", "meterBar")
+        .attr("x", 0)
+        .attr("y", d => yScale(d.vehiclemake))
+        .attr("height", yScale.bandwidth())
+        .attr("width", item => xScale(item[METADATA_xColumnName]));
+
+    svgChartGroup.append("g")
+        .attr("transform", `translate(0, ${chartHeight})`)
+        .attr("class", "xaxis")
+        .call(xAxis);
+
+    svgChartGroup.append("g")
+        .attr("class", "yaxis")
+        .call(yAxis);
+
+    
+    //- Create Labels
+    let labelGroup =  svgChartGroup.append("g");
+
+    labelGroup.selectAll("text")
+        .data(sourceMeterDetails.summarymake)
+        .enter()
+        .append("text")
+        .attr("class", "meterText")
+        .attr("x", 10)
+        .attr("y", d => (yScale(d.vehiclemake) + 22))
+        .text(d => `${d.vehiclemake}: ${d.count}`);
 }
